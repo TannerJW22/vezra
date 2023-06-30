56b17619b54eceb8aa9ae5006491b772bb35434b
TannerJW22 <twcapitalenterprises@gmail.com>
2023-06-30 16:15:50 -0400

    contact schema / validator refactor

--- src/app/dashboard/classes/page.tsx
+++ src/app/dashboard/classes/page.tsx
@@ -1,11 +1,4 @@
-import { MockProject } from "@/app/dashboard/classes/SANDBOX";
-
 // =-=-=- Main Component =-=-=- //
 export default function ClassesPage() {
-	return (
-		<main>
-			Classes
-			<MockProject>Hello from MagicDiv</MockProject>
-		</main>
-	);
+  return <main>Classes</main>;
 }
